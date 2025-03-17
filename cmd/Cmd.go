430111cb166afc569335f6c8250b0583a923baa7
MrNorwood <[email protected]>
2025-03-17 18:26:44 +0100

    some preliminary cli port checking

---
+++ cmd/Cmd.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/urfave/cli/v3"
+	"os"
+)
+
+func Execute() {
+	app := &cli.Command{
+		Name:    "sugarcube-server",
+		Usage:   "Server for the SugarCube coupon extension",
+		Version: "v1.0.0-alpha.1",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:    "db-port",
+				Value:   27017,
+				Usage:   "Mongod port",
+				Aliases: []string{"P"},
+				Action: func(ctx context.Context, cli *cli.Command, port int64) error {
+					uid := os.Geteuid()
+					if port < 1 || port > 65535 {
+						return errors.New("Invalid port: Must be a number between 1 and 65535")
+					} else if port <= 1024 && uid != 0 {
+						return errors.New("Invalid port: Ports 1-1024 require root")
+
+					}
+
+					return nil
+				},
+			},
+		},
+
+		Action: func(ctx context.Context, cli *cli.Command) error {
+			fmt.Println(cli.Int("db-port"))
+
+			return nil
+		},
+	}
+	if err := app.Run(context.Background(), os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
