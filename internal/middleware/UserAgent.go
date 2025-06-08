4f9c010203751dc7e4d765fdc6857cc66a0d1cb2
MrNorwood <[email protected]>
2025-06-08 03:02:47 +0200

    header checker

--- internal/middleware/UserAgent.go
+++ internal/middleware/UserAgent.go
@@ -1,27 +1,41 @@
 package middleware
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/rs/zerolog/log"
+	"fmt"
 	"net/http"
 	"strings"
+
+	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
 )
 
-var USER_AGENT string = "SugarCube/"
+var HEADER string = "SC-Api-version"
+var API_VER string = "v1"
 
 func CheckUserAgent(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
-		ua := ctx.Request().UserAgent()
-		if !strings.HasPrefix(ua, USER_AGENT) {
+		header := ctx.Request().Header
+
+		if strings.Compare(header.Get(HEADER), API_VER) == 0 {
 			log.Warn().
 				Str("ip", ctx.RealIP()).
-				Str("user_agent", ua).
+				Str("header", HeaderToString(header)).
 				Str("path", ctx.Request().URL.Path).
-				Msg("Blocked request due to invalid User-Agent")
+				Msg("Blocked request due to invalid header")
 
 			return ctx.String(http.StatusForbidden, "Forbidden")
 
 		}
 		return next(ctx)
 	}
+}
+
+func HeaderToString(header http.Header) string {
+	var b strings.Builder
+	for k, v := range header {
+		for _, val := range v {
+			b.WriteString(fmt.Sprintf("%s: %s\n", k, val))
+		}
+	}
+	return b.String()
 }
